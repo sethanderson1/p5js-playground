82dd8dd27b49d79777aae42d44aedfc98ac28ca3
Seth Anderson <sdande02@gmail.com>
2020-09-15 04:27:12 -0700

    .

--- src/colorSwarm.js
+++ src/colorSwarm.js
@@ -11,41 +11,43 @@ const sketch = (p) => {
     let height = window.innerHeight;
     let isIncreasing
     let colorVal = 0;
-    
+
     p.setup = () => {
         p.createCanvas(width, height * 0.992);
         p.frameRate(6)
         p.initHex()
     }
 
-    
+
     p.initHex = () => {
 
         makeHexagons()
-        
+
     }
-    
-    
+
+
     p.draw = () => {
         p.background(200);
+
         
 
-        let colorUpperLimit = 200;
-        let colorLowerLimit = 0
-        
-        if (colorVal === colorUpperLimit - 1) {
-            isIncreasing = false
-        } else if (colorVal === colorLowerLimit) {
-            isIncreasing = true
-        }
-        if (isIncreasing) {
-            colorVal = p.frameCount % colorUpperLimit
-        } else {
-            colorVal = colorUpperLimit - p.frameCount % colorUpperLimit - 1
-        }
+
+        // let colorUpperLimit = 200;
+        // let colorLowerLimit = 0
+
+        // if (colorVal === colorUpperLimit - 1) {
+        //     isIncreasing = false
+        // } else if (colorVal === colorLowerLimit) {
+        //     isIncreasing = true
+        // }
+        // if (isIncreasing) {
+        //     colorVal = p.frameCount % colorUpperLimit
+        // } else {
+        //     colorVal = colorUpperLimit - p.frameCount % colorUpperLimit - 1
+        // }
 
     }
-    
+
     function hexagon(x, y, radius, ...color) {
         p.fill(...color[1]);
         p.noStroke();
@@ -63,29 +65,32 @@ const sketch = (p) => {
         constructor(shadeVal) {
             this.shadeVal = shadeVal
         }
-        
-    }
-           const makeHexagons = () => {
-                let space = 50; // x spacing
-                let hexWidth = 30;
-                let colLength = Math.ceil(width / space);
-                let randColor;
-            
-                for (let y = 0; y < colLength; y++) {
-                    let py = y * space * p.sqrt(3) / 2; // y position
-                    for (let x = 0; x < colLength; x++) {
-                        randColor = [p.random(colorVal)]
 
-                        hex[x] = new Hex(randColor)
-            
-                        if (y % 2 === 0) {
-                            hexagon(x * space, py, hexWidth, hexWidth, randColor);
-                        } else {
-                            hexagon(space / 2 + x * space, py, hexWidth, hexWidth, randColor);
-                        }
-                    }
+        
+
+    }
+
+    const makeHexagons = () => {
+        let space = 50; // x spacing
+        let hexWidth = 30;
+        let colLength = Math.ceil(width / space);
+        let randColor;
+        const hex = []
+        for (let y = 0; y < colLength; y++) {
+            let py = y * space * p.sqrt(3) / 2; // y position
+            for (let x = 0; x < colLength; x++) {
+                randColor = [p.random(colorVal)]
+
+                hex[x] = new Hex(randColor)
+
+                if (y % 2 === 0) {
+                    hexagon(x * space, py, hexWidth, hexWidth, randColor);
+                } else {
+                    hexagon(space / 2 + x * space, py, hexWidth, hexWidth, randColor);
                 }
             }
+        }
+    }
 
 }
 
