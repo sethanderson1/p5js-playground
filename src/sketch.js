ba456543bf24175391d0eb501592f6a3b78f5858
Seth Anderson <sdande02@gmail.com>
2020-12-27 04:44:20 -0700

    init

--- src/sketch.js
+++ src/sketch.js
@@ -6,7 +6,7 @@ const sketch = (p) => {
     const bars = [];
     p.setup = () => {
         // p.frameRate(1);
-        for (let i = 0; i < 30; i++) {
+        for (let i = 0; i < 100; i++) {
             bars[i] = new Bar();
         }
         p.createCanvas(p.windowWidth, p.windowHeight);
@@ -19,22 +19,40 @@ const sketch = (p) => {
             bars[i].createBar();
             bars[i].moveBar();
         }
-        // console.log('bars', bars)
     }
 
     const options = {
         Colors: {
-            color1: '#4a4e4d',
-            color2: '#0e9aa7',
-            color3: '#3da4ab',
-            color4: '#f6cd61',
-            color5: '#fe8a71',
+            color1: '#4a4e4d33',
+            color2: '#0e9aa733',
+            color3: '#3da4ab33',
+            color4: '#f6cd6133',
+            color5: '#fe8a7133',
         }
+    }
+
+    const xSlots = (numSlots) => {
+        const slots = [];
+        for (let i = 0; i < numSlots; i++) {
+
+            slots.push(p.windowWidth * i / numSlots)
+        }
+        return slots;
+    }
+    const ySlots = (numSlots) => {
+        const slots = [];
+        for (let i = 0; i < numSlots; i++) {
+
+            slots.push(p.windowHeight * i / numSlots)
+        }
+        return slots;
     }
     class Bar {
         constructor() {
-            this.xPos = p.random(0, p.windowWidth);
-            this.yPos = p.random(0, p.windowHeight);
+            // this.xPos = p.random(0, p.windowWidth);
+            // this.yPos = p.random(0, p.windowHeight);
+            this.xPos = p.random(xSlots(8));
+            this.yPos = p.random(ySlots(6));
             this.xVel = p.random(1, 12);
             this.yVel = p.random(1, 12);
             this.direction = p.random(-1, 1) > 0 ? 'horizontal' : 'vertical';
