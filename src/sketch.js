cadc08ad7a27960cf146bd4a731123cae8e8ffde
Seth Anderson <sdande02@gmail.com>
2020-12-28 14:59:09 -0700

    inspected

--- src/sketch.js
+++ src/sketch.js
@@ -3,20 +3,26 @@
 const sketch = (p) => {
     let flower = [];
     let bg;
-    let COLS = createCols("https://coolors.co/fed167-f5f2ea-a29fbc-5f6591-f3453f-e02f10-c32f18");
+    let COLS = ['#F3F2DB', '#E8614F', '#79C3A7', '#4B3331', '#668065'];
+    // let COLS = createCols("https://coolors.co/fed167-f5f2ea-a29fbc-5f6591-f3453f-e02f10-c32f18");
     let SCOL = COLS.shift();
-    let s = p.min(p.windowWidth, p.windowHeight) * 0.99;
+    let s = p.min(p.windowWidth, p.windowHeight) * 0.8;
     let width = s;
     let height = s;
-    
+    // let width = s;
+    // let height = s;
+
     p.setup = () => {
-        p.frameRate(30);
+        p.frameRate(22);
+        // p.frameRate(1);
+        // p.createCanvas(2000,2000);
         p.createCanvas(s, s);
         let c = 0;
         let r = p.min(width, height);
         while (r > 0) {
             let f = new SpreadFlower(r, c % 2 === 0);
             flower.push(f);
+            console.log('flower', flower)
             c++;
             r -= 30;
         }
@@ -30,24 +36,33 @@ const sketch = (p) => {
             let s = p.noise(x * 0.01, y * 0.01) * 1 + 1;
             bg.rect(x, y, s, s);
         }
+        // paper function might be better
     }
 
-    p.draw=() =>{
-        p.background(100);
-        p.push();
-        p.translate(width / 2, height / 2);
-        p.rotate(p.frameCount / 50);
-        for (const f of flower) {
-            f.update();
-            f.display();
+    p.draw = () => {
+        if (p.frameCount < 222222) {
+
+            p.background(100);
+            p.push();
+            p.translate(width / 2, height / 2);
+            p.rotate(p.frameCount / 150);
+            for (const f of flower) {
+                f.update();
+                f.display();
+            }
+            p.pop();
+            if (flower[1].isFullSize()) {
+                flower.shift();
+                let useRotateOffset = !flower[flower.length - 1].useRotateOffset;
+                flower.push(new SpreadFlower(1, useRotateOffset));
+            }
+            // p.push();
+            // p.rotate(p.frameCount / 100);
+
+            p.image(bg, 0, 0);
+            // p.pop();
         }
-        p.pop();
-        if (flower[1].isFullSize()) {
-            flower.shift();
-            let useRotateOffset = !flower[flower.length - 1].useRotateOffset;
-            flower.push(new SpreadFlower(1, useRotateOffset));
-        }
-        p.image(bg, 0, 0);
+
     }
 
 
@@ -65,6 +80,8 @@ const sketch = (p) => {
 
         update() {
             const ns = 0.002;
+            // const ns = -999999;
+            // this.radius += p.random([1,1,1,1,2,2,3]);
             this.radius += 1 + p.noise(this.radius + p.frameCount * ns, (p.frameCount + 100) * ns) * 5;
         }
 
@@ -87,7 +104,7 @@ const sketch = (p) => {
         const r = p.HALF_PI - radSpan / 2;
         if (r >= p.HALF_PI) return;
 
-        const circleRadius = radius *p.cos(r);
+        const circleRadius = radius * p.cos(r);
         let xOff = p.cos(radSpan / 2) * circleRadius;
         let yOff = p.sin(radSpan / 2) * circleRadius * -1;
         let radOff = rotateOffset === true ? radSpan / 2 : 0;
@@ -116,33 +133,17 @@ const sketch = (p) => {
         }
     }
 
-    function createCols(_url) {
-        let slash_index = _url.lastIndexOf('/');
-        let pallate_str = _url.slice(slash_index + 1);
-        let arr = pallate_str.split('-');
-        for (let i = 0; i < arr.length; i++) {
-            arr[i] = '#' + arr[i];
-        }
-        return arr;
-    }
+    // function createCols(_url) {
+    //     let slash_index = _url.lastIndexOf('/');
+    //     let pallate_str = _url.slice(slash_index + 1);
+    //     let arr = pallate_str.split('-');
+    //     for (let i = 0; i < arr.length; i++) {
+    //         arr[i] = '#' + arr[i];
+    //     }
+    //     return arr;
+    // }
 }
 
 
 
 export default sketch;
-
-
-
-
-
-const options = {
-    BarColors: {
-        color1: '#4a4e4d',
-        color2: '#0e9aa7',
-        color3: '#3da4ab',
-        color4: '#f6cd61',
-        color5: '#fe8a71',
-    },
-    BackgroundColor: '#fff',
-}
-
